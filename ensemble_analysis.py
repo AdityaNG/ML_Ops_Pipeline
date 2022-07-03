74354b58a6b46b3763674ebe2d4fc15847d2e120
Aditya <adityang5@gmail.com>
2022-07-03 10:57:27 +0530

    Update ensemble_analysis.py

--- ensemble_analysis.py
+++ ensemble_analysis.py
@@ -10,13 +10,62 @@ against the dataset ground truth
 import glob
 import pickle
 import os
-
+import inspect
+from datetime import datetime
 import time
 
+import json
+
 from constants import MODEL_TESTING, ENSEMBLE_TESTING, DATASET_DIR
-from obj_det_demo import all_inputs
+from all_pipelines import get_all_inputs
+from history import local_history
 
 def main():
+	loc_hist = local_history(__file__)
+	task_list = {}
+	all_inputs = get_all_inputs()
+
+	for pipeline_name in all_inputs:
+		all_dataset_dir = DATASET_DIR.format(pipeline_name=pipeline_name)
+		interpreters = all_inputs[pipeline_name].get_pipeline_dataset_interpreter()
+		for interpreter_name in interpreters:
+			interpreter_dataset_dir = os.path.join(all_dataset_dir, interpreter_name)
+			interpreter_datasets = glob.glob(os.path.join(interpreter_dataset_dir,"*"))
+			for dataset_dir in interpreter_datasets:
+				ensemble_classes = all_inputs[pipeline_name].get_pipeline_ensembler()
+				
+				model_classes = all_inputs[pipeline_name].get_pipeline_model()
+				model_predictions = {}
+				
+				model_did_change = False
+				for model_name in model_classes:
+					model_file_path = inspect.getfile(model_classes[model_name])
+					model_last_modified = datetime.fromtimestamp(os.path.getmtime(model_file_path))
+					model_task_id = "MODEL_" + model_name + ":"+ interpreter_name + ":" + dataset_dir
+					if loc_hist[model_task_id] != model_last_modified:
+						model_did_change = True
+						break
+				
+				if model_did_change:
+					for ensembler_name in ensemble_classes:
+						ensemble_file_path = inspect.getfile(ensemble_classes[ensembler_name])
+						ensemble_last_modified = datetime.fromtimestamp(os.path.getmtime(ensemble_file_path))
+						task_id = model_name + ":"+ interpreter_name + ":" + dataset_dir
+						if loc_hist[task_id] != ensemble_last_modified:
+							task_list.setdefault(pipeline_name, {})
+							task_list[pipeline_name].setdefault(interpreter_name, {})
+							task_list[pipeline_name][interpreter_name].setdefault(dataset_dir, {})
+							task_list[pipeline_name][interpreter_name][dataset_dir].setdefault(ensembler_name, task_id)
+
+	if task_list == {}:
+		print("Waiting for new tasks...")
+		time.sleep(5)
+		return
+
+	print("-"*10)
+	print("Task list:\n", json.dumps(task_list, sort_keys=True, indent=4))
+	print("-"*10)
+
 	for pipeline_name in all_inputs:
 		all_dataset_dir = DATASET_DIR.format(pipeline_name=pipeline_name)
 		interpreters = all_inputs[pipeline_name].get_pipeline_dataset_interpreter()
@@ -30,10 +79,7 @@ def main():
 				model_classes = all_inputs[pipeline_name].get_pipeline_model()
 				model_predictions = {}
 				for model_name in model_classes:
-					#print("-"*os.get_terminal_size().columns)
-					#print("model_name:\t",model_name)
-					#print("interpreter_name:\t",interpreter_name)
-					#print("dataset_dir:\t",dataset_dir)
+
 					testing_dir = MODEL_TESTING.format(pipeline_name=pipeline_name, interpreter_name=interpreter_name, model_name=model_name)
 					os.makedirs(testing_dir, exist_ok=True)
 					results_pkl = os.path.join(testing_dir, "results.pkl")
@@ -50,7 +96,8 @@ def main():
 					model_predictions[model_name] = predictions
 
 				for ensembler_name in ensemble_classes:
-					print("-"*os.get_terminal_size().columns)
+					
+					print("-"*10)
 					print("ensembler_name:\t",ensembler_name)
 					print("interpreter_name:\t",interpreter_name)
 					print("dataset_dir:\t",dataset_dir)
@@ -70,7 +117,9 @@ def main():
 					ens_predictions_handle = open(ens_predictions_pkl, 'wb')
 					pickle.dump(ens_predictions, ens_predictions_handle, protocol=pickle.HIGHEST_PROTOCOL)
 					ens_predictions_handle.close()
-					print("-"*os.get_terminal_size().columns)
+
+					task_id = task_list[pipeline_name][interpreter_name][dataset_dir][ensembler_name]
+					loc_hist[task_id] = model_last_modified
 
 if __name__ == "__main__":
 	import traceback
@@ -88,9 +137,9 @@ if __name__ == "__main__":
 		try:
 			main()
 		except Exception as ex:
-			print("-"*os.get_terminal_size().columns)
+			print("-"*10)
 			print("An error has occured")
 			print(ex)
 			traceback.print_exc()
-			print("-"*os.get_terminal_size().columns)
+			print("-"*10)
 			time.sleep(1)
